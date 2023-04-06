f2563b8b9c35cc0c55e604f6e5eabaf775aa3473
Robert C <oglejsi2@gmail.com>
2023-04-06 21:35:40 +0200

    Create learning_01_02_prompt.py

---
+++ learning_01_02_prompt.py
@@ -0,0 +1,6 @@
+#Lets take another step
+#1. The program will ask us what is your name with a command named input
+#2  It will store the name we entered into variable called vname. This is a little box storing the value for later use
+#3  At the last step the program will return nice greeting using varible vname.
+vname = input('What is your name? ')   #ask about a name and store it to variable
+print("Hello " + vname)              #as you can see we are using print command from previous lesson
