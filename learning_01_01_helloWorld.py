2a1bbc63b5c9a53312bebfde099ef1ef5fd68944
Robert C <oglejsi2@gmail.com>
2023-04-06 21:34:29 +0200

    Create learning_01_01_helloWorld.py

---
+++ learning_01_01_helloWorld.py
@@ -0,0 +1,5 @@
+#Let's write our first program. It has to be Hello world program.
+#This is very simple program that writes Hello world on your screen
+#Programing languages are a bunch of commands that instructs the computer what to do
+#One of such commands is print. Print command prints something on screen.
+print("Hello World")
